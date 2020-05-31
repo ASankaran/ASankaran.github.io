2b1459fd97731e2b0b9b2740cbc9e888b9e8f948
Arnav Sankaran <arnavsankaran@gmail.com>
2020-05-31 17:09:52 -0500

    Add content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Paper, Container, Avatar, Typography, IconButton, Link } from '@material-ui/core';
+import { LinkedIn, GitHub } from '@material-ui/icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      position: 'absolute', left: '50%', top: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+      <Container maxWidth="sm">
+        <Paper>
+          <Grid container justify="center" alignItems="center" spacing={2}>
+            <Grid item xs={12}>
+              <Container>
+                <span style={{
+                  display: 'flex', justifyContent: 'center', alignItems: 'center'
+                }}>
+                  <Avatar alt="Arnav Sankaran" src="/assets/headshot.jpg" style={{
+                    height: 200, width: 200
+                  }} />
+                </span>
+                <Typography variant="h2" align="center">
+                  Arnav Sankaran
+                </Typography>
+              </Container>
+            </Grid>
+            <Grid item xs={12}>
+              <Container>
+                <Typography variant="body1" align="center">
+                  Hi, I'm Arnav a recent graduate of the University of Illinois at Urbana-Champaign.
+                  I will be joining Hudson River Trading in August as a Core Developer.
+                  I have previously worked at Facebook, Uber, and Ocient as a Software Engineering Intern.
+                </Typography>
+              </Container>
+            </Grid>
+            <Grid item xs={12}>
+              <Container>
+                <Typography variant="body1" align="center">
+                  My interests lie primarly in designing and building high performance software, distributed systems, networking, and architecture.
+                </Typography>
+              </Container>
+            </Grid>
+            <Grid item xs={12}>
+              <Container>
+                <Typography variant="body1" align="center">
+                  You can learn more about my work experience the projects I have built over the years through my LinkedIn and GitHub profiles.
+                </Typography>
+              </Container>
+            </Grid>
+            <Grid item xs={12}>
+              <Container>
+                <span style={{
+                  display: 'flex', justifyContent: 'center', alignItems: 'center'
+                }}>
+                  <Link href="https://www.linkedin.com/in/arnav-sankaran/" target="_blank">
+                    <IconButton>
+                      <LinkedIn htmlColor="rgb(14, 118, 168)" />
+                    </IconButton>
+                  </Link>
+                  <Link href="https://github.com/ASankaran" target="_blank">
+                    <IconButton>
+                      <GitHub htmlColor="rgb(33, 31, 31)" />
+                    </IconButton>
+                  </Link>
+                </span>
+              </Container>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
     </div>
   );
 }
